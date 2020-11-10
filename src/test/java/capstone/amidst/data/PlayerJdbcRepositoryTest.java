2fffd1124a3be83a691acae91043212ed9ab07a3
Alexander Bearden <eabearden97@aol.com>
2020-11-10 14:47:51 -0600

    Setup test file for PlayerJdbcRepository.

--- src/test/java/capstone/amidst/data/PlayerJdbcRepositoryTest.java
+++ src/test/java/capstone/amidst/data/PlayerJdbcRepositoryTest.java
@@ -1,10 +1,14 @@
 package capstone.amidst.data;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerJdbcRepositoryTest {
+
+    @Autowired
+    PlayerJdbcRepository repository;
 
     @Test
     void findAll() {
