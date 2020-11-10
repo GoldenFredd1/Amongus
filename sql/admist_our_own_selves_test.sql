0f12a130c81479ed15adb5b31ffd7f132c6c44a4
Alexander Bearden <eabearden97@aol.com>
2020-11-10 16:04:59 -0600

    updated test sql file

--- sql/admist_our_own_selves_test.sql
+++ sql/admist_our_own_selves_test.sql
@@ -2,46 +2,52 @@ drop database if exists amidst_our_own_selves_test;
 create database amidst_our_own_selves_test;
 use amidst_our_own_selves_test;
 
-
-
-CREATE TABLE IF NOT EXISTS app_user (
+-- -----------------------------------------------------
+-- Table `amidst_our_own_selves_test`.`app_user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `amidst_our_own_selves_test`.`app_user` (
   `app_user_id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `password_hash` VARCHAR(45) NOT NULL,
   `disabled` TINYINT NOT NULL,
   PRIMARY KEY (`app_user_id`),
-  UNIQUE INDEX `app_user_id_UNIQUE` (`app_user_id` ASC) VISIBLE)
+	INDEX `app_user_id_UNIQUE` (`app_user_id` ASC) VISIBLE)
 ENGINE = InnoDB;
 
-
-CREATE TABLE IF NOT EXISTS app_role (
+-- -----------------------------------------------------
+-- Table `amidst_our_own_selves_test`.`app_role`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `amidst_our_own_selves_test`.`app_role` (
   `app_role_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   UNIQUE INDEX `app_role_id_UNIQUE` (`app_role_id` ASC) VISIBLE,
   PRIMARY KEY (`app_role_id`))
 ENGINE = InnoDB;
 
-
-CREATE TABLE IF NOT EXISTS app_user_role (
+-- -----------------------------------------------------
+-- Table `amidst_our_own_selves_test`.`app_user_role`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `amidst_our_own_selves_test`.`app_user_role` (
   `app_user_id` INT NOT NULL,
   `app_role_id` INT NOT NULL,
   INDEX `fk_app_user_role_app_user_id_idx` (`app_user_id` ASC) VISIBLE,
   INDEX `fk_app_user_role_app_role_role_id_idx` (`app_role_id` ASC) VISIBLE,
   CONSTRAINT `fk_app_user_role_app_user_id`
     FOREIGN KEY (`app_user_id`)
-    REFERENCES app_user (`app_user_id`)
+    REFERENCES `amidst_our_own_selves_test`.`app_user` (`app_user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_app_user_role_app_role_role_id`
     FOREIGN KEY (`app_role_id`)
-    REFERENCES app_role (`app_role_id`)
+    REFERENCES `amidst_our_own_selves_test`.`app_role` (`app_role_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
-
-CREATE TABLE IF NOT EXISTS Player (
+-- -----------------------------------------------------
+-- Table `amidst_our_own_selves_test`.`Player`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `amidst_our_own_selves_test`.`Player` (
   `playerId` INT NOT NULL AUTO_INCREMENT,
   `playerName` VARCHAR(45) NOT NULL,
   `isDead` TINYINT NOT NULL,
@@ -51,22 +57,25 @@ CREATE TABLE IF NOT EXISTS Player (
   INDEX `fk_player_app_user_role_id_idx` (`app_user_id` ASC) VISIBLE,
   CONSTRAINT `fk_player_app_user_role_id`
     FOREIGN KEY (`app_user_id`)
-    REFERENCES app_user_role (`app_user_id`)
+    REFERENCES `amidst_our_own_selves_test`.`app_user_role` (`app_user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
-CREATE TABLE IF NOT EXISTS Room (
+-- -----------------------------------------------------
+-- Table `amidst_our_own_selves_test`.`Room`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `amidst_our_own_selves_test`.`Room` (
   `roomId` INT NOT NULL,
   `roomName` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`roomId`),
   UNIQUE INDEX `roomName_UNIQUE` (`roomName` ASC) VISIBLE)
 ENGINE = InnoDB;
 
-
-
-CREATE TABLE IF NOT EXISTS Task (
+-- -----------------------------------------------------
+-- Table `amidst_our_own_selves_test`.`Task`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `amidst_our_own_selves_test`.`Task` (
   `taskId` INT NOT NULL,
   `taskName` VARCHAR(45) NOT NULL,
   `roomId` INT NOT NULL,
@@ -75,13 +84,15 @@ CREATE TABLE IF NOT EXISTS Task (
   INDEX `fk_task_room_id_idx` (`roomId` ASC) VISIBLE,
   CONSTRAINT `fk_task_room_id`
     FOREIGN KEY (`roomId`)
-    REFERENCES Room (`roomId`)
+    REFERENCES `amidst_our_own_selves_test`.`Room` (`roomId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
-CREATE TABLE IF NOT EXISTS Game (
+-- -----------------------------------------------------
+-- Table `amidst_our_own_selves_test`.`Game`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `amidst_our_own_selves_test`.`Game` (
   `gameId` INT NOT NULL,
   `playerId` INT NOT NULL,
   `roomId` INT NOT NULL,
@@ -89,19 +100,20 @@ CREATE TABLE IF NOT EXISTS Game (
   INDEX `fk_game_player_id_idx` (`playerId` ASC) VISIBLE,
   CONSTRAINT `fk_game_player_id`
     FOREIGN KEY (`playerId`)
-    REFERENCES Player (`playerId`)
+    REFERENCES `amidst_our_own_selves_test`.`Player` (`playerId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_game_room_id`
     FOREIGN KEY (`roomId`)
-    REFERENCES Room (`roomId`)
+    REFERENCES `amidst_our_own_selves_test`.`Room` (`roomId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
-
-CREATE TABLE IF NOT EXISTS Player_Assigned_Task (
+-- -----------------------------------------------------
+-- Table `amidst_our_own_selves_test`.`Player_Assigned_Task`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `amidst_our_own_selves_test`.`Player_Assigned_Task` (
   `task_id` INT NOT NULL,
   `playerId` INT NOT NULL,
   `isComplete` TINYINT NOT NULL,
@@ -109,12 +121,12 @@ CREATE TABLE IF NOT EXISTS Player_Assigned_Task (
   INDEX `fk_player_assigned_task_player_id_idx` (`playerId` ASC) VISIBLE,
   CONSTRAINT `fk_player_assigned_task_task_id`
     FOREIGN KEY (`task_id`)
-    REFERENCES Task (`taskId`)
+    REFERENCES `amidst_our_own_selves_test`.`Task` (`taskId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_player_assigned_task_player_id`
     FOREIGN KEY (`playerId`)
-    REFERENCES Player (`playerId`)
+    REFERENCES `amidst_our_own_selves_test`.`Player` (`playerId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -123,24 +135,40 @@ delimiter //
 create procedure set_known_good_state()
 begin
 
+-- delete from location;
+--     alter table location auto_increment = 1;
+--     delete from agency_agent;
+-- 	delete from agency;
+-- 	alter table agency auto_increment = 1;
+--     delete from agent;
+--     alter table agent auto_increment = 1;
+delete from Player;
+	alter table Player auto_increment = 1;
+delete from app_user_role;
+delete from app_role;
+	alter table app_role auto_increment = 1;
+delete from app_user;
+	alter table app_user auto_increment = 1;
+
 insert into app_user values
-(1,'Bob the test player','hashpasswordgoeshere',false);
+(1,'Bob the test player','hashpasswordgoeshere',false),
+(2,'Computer','hashpasswordgoeshere',false);
 
 insert into app_role values
 (1,'Admin'),
-(2,'User');
+(2,'User'),
+(3,'Computer');
 
 insert into app_user_role values
-(1,2);
+(1,2),
+(2,3);
 
 insert into Player(playerId, playerName, isDead, isImposter, app_user_id) values
 (1,'testPlayerAlpha',false, false,1),
-(2,'Computer1',false, false,null),
-(3,'Computer2',false, false,null),
-(4,'Computer3',false, false,null);
+(2,'Computer1',false, false,2),
+(3,'Computer2',false, false,2),
+(4,'Computer3',false, false,2);
 
-    
-    
 end //
 -- 4. Change the statement terminator back to the original.
 delimiter ;
@@ -155,7 +183,6 @@ insert into Room(roomId,roomName) values
 (6,'Neiman Marcus'),
 (7,'The Hallway of Failure');
 
-
 insert into Task(taskId,taskName,roomId) values
 (1,'Pickup trash',1),
 (2,'Collect moldy sandwich',1),
@@ -169,3 +196,6 @@ insert into Task(taskId,taskName,roomId) values
 (10,'Check for usable batteries',5),
 (11,'Find a fancy hat',6),
 (12,'Put makeup on a mannequin',6);
+
+select playerId, playerName, isDead, isImposter, app_user_id
+from Player;
